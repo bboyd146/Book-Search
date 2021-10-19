964936dcffc07cc24394adb709cea8ac2545453f
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-19 17:01:43 -0500

    type User property set up

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,21 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql `
-type Book {
-    _id: ID
-    authors: String
-    description: String!
-    bookId: String!
-    image: String
-    link: String
-    title: String!
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
 }
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+    }
 `;
 
 module.exports = typeDefs
