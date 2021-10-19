d67eb7a70d4f442912b37e8760046b940e7d50af
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2021-10-19 16:46:08 -0500

    setting up ApolloServer

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
